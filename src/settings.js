768f3ed655bc360d87cf98bd8fa4b11e9220ef57
liutao <1710959781@qq.com>
2023-12-29 00:23:34 +0800

    main

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '嵌入式硬件实训教学系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
